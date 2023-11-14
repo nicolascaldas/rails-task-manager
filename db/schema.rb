c7a60420f0e5ea92bc6736d1ca30fd99e29b83fe
Nicolas Caldas <nicolascaldas10@gmail.com>
2023-11-14 16:14:41 -0300

    ugly but base code is there

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_14_154822) do
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_160628) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.text 'details'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.boolean 'completed', default: false
   end
-
 end
